e2c09588fdcb8c770b60c9a680eec2a475eb35f0
Shanjid Hasan Nishat <48083980+shanjidhasan@users.noreply.github.com>
2024-03-02 02:22:47 +0600

    Create 933. Number of Recent Calls.py

---
+++ 933. Number of Recent Calls.py
@@ -0,0 +1,21 @@
+class RecentCounter(object):
+
+    def __init__(self):
+        self.requests = deque()
+
+    def ping(self, t):
+        """
+        :type t: int
+        :rtype: int
+        """
+        self.requests.append(t)
+        
+        while self.requests and self.requests[0] < t - 3000:
+            self.requests.popleft()
+
+        return len(self.requests)
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
